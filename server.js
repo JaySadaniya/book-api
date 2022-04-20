42ff43204c02e3ef8241ebcea2ea4c369fe7d5af
Jay <sadaniya1011@gmail.com>
2022-04-20 21:35:35 +0530

    change layout

--- server.js
+++ server.js
@@ -36,14 +36,10 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(passport.setAuthenticatedUser);
 
-app.use(require('express-ejs-layouts'));
-
 // Tell app to use assets upon different request
 app.use("/", express.static(env.assets_path));
 app.use("/users", express.static(env.assets_path));
 app.use("/users/bookdetail", express.static(env.assets_path));
-
-app.set('layout extractScripts', true);
 
 app.get('/', usersController.home);
 
